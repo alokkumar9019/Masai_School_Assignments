971205b37e926966e3dbfb0c2ee0595af4bf91f7
alokkumar9019 <singhalokkumar459@gmail.com>
2025-09-06 14:59:29 +0530

    a

--- Photo-Gallery-app/src/components/Gallery.jsx
+++ Photo-Gallery-app/src/components/Gallery.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 function Gallery({photos, onSelect}) {
+    if(!photos || photos.length===0){
+        return <p>No photos Added yet. Add one!</p>
+    }
   return (
     <div>
+<p>hello</p>
         {photos.map((p)=>{
             <img
                 key={p.id}
@@ -16,4 +20,4 @@ function Gallery({photos, onSelect}) {
   )
 }
 
-export default Gallery
+export default Gallery;
